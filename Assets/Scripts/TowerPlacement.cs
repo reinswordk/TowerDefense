43837c89e8cc5f77b00b0f28aadf80d2ec7addbf
Muhamad Hariyanto <reinswordk@gmail.com>
2021-09-19 18:00:40 +0700

    First Commit

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+
+    private Tower _placedTower;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (_placedTower != null && _placedTower.isPlaced && !_placedTower.gameObject.activeSelf) {
+            _placedTower = null;
+        }
+
+        if (_placedTower == null)
+        {
+            Tower tower = collision.GetComponent<Tower>();
+            if (tower != null)
+            {
+                tower.SetPlacePosition(transform.position);
+                _placedTower = tower;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (_placedTower == null) return;
+
+        if (!_placedTower.isPlaced)
+        {
+            _placedTower.SetPlacePosition(null);
+            _placedTower = null;
+        }
+    }
+}
